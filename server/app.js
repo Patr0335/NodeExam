c16370273c4ef6208871148e0d350ee8c71de36f
patr0335 <patr0335@stud.kea.dk>
2022-06-28 09:45:01 +0200

    baselimiter

--- server/app.js
+++ server/app.js
@@ -21,16 +21,16 @@ app.use(
 );
 
 //################# rate-limit middleware
-// import rateLimit from "express-rate-limit";
+import rateLimit from "express-rate-limit";
 
-// const baseLimiter = rateLimit({
-//   windowMs: 15 * 60 * 1000, // 15 minutes
-//   max: 10, // Limit each IP to 10 requests per `window` (here, per 15 minutes)
-//   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-//   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-// });
+const baseLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10, // Limit each IP to 10 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
-// app.use(baseLimiter); // ddos prevention
+app.use(baseLimiter); // ddos prevention
 
 //################# router
 import userRouter from "./router/userRouter.js";
